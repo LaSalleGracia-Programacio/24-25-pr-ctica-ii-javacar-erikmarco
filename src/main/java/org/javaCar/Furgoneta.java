72816aa061bdda1d14d3dfa21aae6fb194f7ca0c
Erik <erikmuar91@gmail.com>
2025-03-17 09:48:40 +0100

    clase furgoneta

---
+++ src/main/java/org/javaCar/Furgoneta.java
@@ -0,0 +1,22 @@
+package org.javaCar;
+
+public class Furgoneta extends Vehicle implements Llogable{
+    public int capacitatCarga;
+
+    @Override
+    public double calcularPreuFurgoneta(int dies, double preuBase, int capacitatCarga){
+        if(capacitatCarga>1000){
+            return dies*preuBase+ 10*dies ;
+        }
+        return dies*preuBase;
+    }
+
+    public int getCapacitatCarga(){
+        return capacitatCarga;
+
+    }
+    public void setCapacitatCarga(int capacitatCarga){
+        this.capacitatCarga = capacitatCarga;
+    }
+}
+
