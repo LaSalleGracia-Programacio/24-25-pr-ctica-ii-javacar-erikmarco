3ed4ea01ebd62178436e7206dec2fe5bfb610beb
Erik <erikmuar91@gmail.com>
2025-03-26 18:33:17 +0100

    Primer menu

--- src/main/java/org/javaCar/Main.java
+++ src/main/java/org/javaCar/Main.java
@@ -44,22 +44,49 @@ public class Main {
                             String tipus = persona.getTipusPersona();
 
                             if (tipus.equals("Administrador")) {
-                                System.out.println("\n--- MENÚ ADMINISTRADOR ---");
+                                System.out.println("--- MENÚ ADMINISTRADOR ---");
                                 System.out.println("1. Afegir un vehicle nou");
-                                System.out.println("2. Veure llista d’usuaris");
-                                System.out.println("3. Consultar llista de vehicles");
-                                System.out.println("4. Tancar sessió");
+                                System.out.println("2. Modificar un vehicle ja existent");
+                                System.out.println("3. Eliminar vehicle");
+                                System.out.println("4. Consultar llista de vehicles");
+                                System.out.println("5. Gestió d’usuaris");
+                                System.out.println("5. Consultar balanç de beneficis");
+                                System.out.println("6. Tancar sessió");
                                 System.out.print("Opció: ");
                                 String opcioAdmin = scanner.nextLine();
 
                                 switch (opcioAdmin) {
                                     case "1":
-                                        System.out.println("Funció per afegir vehicle (pendent d'implementar)");
+                                        System.out.println("");
                                         break;
                                     case "2":
-                                        System.out.println("Funció per veure usuaris (pendent d'implementar)");
+                                        System.out.println("");
                                         break;
                                     case "3":
+                                        System.out.println("");
+                                        break;
+                                    case "4":
+                                        System.out.println("");
+                                        break;
+                                    case "5":
+                                        System.out.println("Benvingut al gestor d'usuaris");
+                                        System.out.println("1. Veure llista d'usuaris");
+                                        System.out.println("2. Eliminar usuari");
+                                        System.out.print("Opció: ");
+                                        String opcioGestor = scanner.nextLine();
+
+                                        switch (opcioGestor){
+                                            case "1":
+                                                System.out.println("");
+                                                break;
+
+                                            case "2":
+                                                System.out.println("");
+                                                break;
+                                        }
+                                        break;
+
+                                    case "6":
                                         System.out.println("Sessió tancada.");
                                         tancarSessio = true;
                                         break;
@@ -68,16 +95,18 @@ public class Main {
                                 }
 
                             } else if (tipus.equals("Usuari")) {
-                                System.out.println("\n--- MENÚ USUARI ---");
-                                System.out.println("1. Veure vehicles disponibles");
+                                System.out.println("--- MENÚ USUARI ---");
+                                System.out.println("1. Buscar vehicles disponibles per llogar");
                                 System.out.println("2. Llogar un vehicle");
-                                System.out.println("3. Tancar sessió");
+                                System.out.println("3. Mirar historial de lloguers");
+                                System.out.println("4. Tancar sessió");
                                 System.out.print("Opció: ");
                                 String opcioUsuari = scanner.nextLine();
 
                                 switch (opcioUsuari) {
                                     case "1":
-                                        System.out.println("Funció per veure vehicles (pendent d'implementar)");
+                                        System.out.println("Benvingut al buscador");
+                                        System.out.println();
                                         break;
                                     case "2":
                                         System.out.println("Funció per llogar vehicle (pendent d'implementar)");
